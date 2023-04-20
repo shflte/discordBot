4afa0ed1c06a3935ad209ccf2e0f5fb168e208be
YC CHEN <shflte1314527@gmail.com>
2023-04-20 17:20:57 +0800

    fix append deque bug

--- app/generate_chatgpt_response.py
+++ app/generate_chatgpt_response.py
@@ -10,11 +10,11 @@ load_dotenv(dotenv_path)
 
 openai.api_key = os.getenv('OPENAI_TOKEN')
 
-history = deque(maxlen=20)
+history = deque(maxlen=50)
 
 def generate_gpt_response(user_input, hint_message_list):
     global history
-    messages = [{"role": "system", "content": "你是進擊的巨人中的里維兵長，講話很兇。你話不多。你的語氣兇狠輕蔑。你只說中文。語氣很差。"}] + history
+    messages = [{"role": "system", "content": "你是進擊的巨人中的里維兵長，講話很兇。你話不多。你的語氣兇狠輕蔑。你只說中文。語氣很差。"}]
     hint_obj_list = [{"role": "system", "content": msg} for msg in hint_message_list]
     messages += hint_obj_list
     history.append({"role": "user", "content": user_input})
