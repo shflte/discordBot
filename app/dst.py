dc9c8f09217a8d4ffcc470e967b4e25e6d29d063
YC CHEN <shflte1314527@gmail.com>
2023-04-22 23:15:04 +0800

    add all_save roll_back toggle features

---
+++ app/dst.py
@@ -0,0 +1,50 @@
+import subprocess
+import os
+
+dst_folder = "/home/shflte/.klei/DoNotStarveTogether"
+
+def back_up(message):
+    # Check if message contains spaces
+    if ' ' in message:
+        print("Error: message cannot contain spaces.")
+        return -1
+    # Construct command to execute
+    command = dst_folder + "/backup.sh -d {}".format(message)
+    # Execute command
+    subprocess.run(command, shell=True)
+    return
+
+def roll_back(save):
+    # Check if message contains spaces
+    if ' ' in save:
+        print("Error: message cannot contain spaces.")
+        return -1
+    # Construct command to execute
+    if save == None:
+        command = dst_folder + "/rollback.sh"
+    else:
+        if save not in all_save():
+            return -1
+        command = dst_folder + "/rollback.sh -s {}".format(save)
+    # Execute command
+    subprocess.run(command, shell=True)
+    return
+
+def toggle(action):
+    if action == "up":
+        command = dst_folder + "/toggle_server.sh -u"
+    elif action == "down":
+        command = dst_folder + "/toggle_server.sh -d"
+
+    # Execute command
+    subprocess.run(command, shell=True)
+    return
+
+def all_save():
+    backup_dir = dst_folder + "/world_backup"
+    folders = os.listdir(backup_dir)
+    folders.sort(reverse=True)
+    result = []
+    for folder in folders[:10]:
+        result.append(folder)
+    return result
--- app/process_cmd.py
+++ app/process_cmd.py
@@ -1,6 +1,6 @@
 # from app.get_recent_plays import get_recent_plays
 from app.tools import is_wee
-from app.backup import backup
+from app.dst import back_up, toggle, roll_back, all_save
 
 async def process_cmd(message, cmd, switch):
     if not is_wee(message.author.roles):
@@ -17,20 +17,56 @@ async def process_cmd(message, cmd, switch):
         await message.channel.send("古摸寧")
         return
     
-    if "backup" in cmd:
-        backup_message = cmd.split(' ')[1]
-        if ' ' in backup_message:
-            await message.channel.send("不能有空格")
+    if len(cmd.split()) >= 2 and cmd.split()[0] == "dst":
+        dst_cmd = cmd.split()[1]
+        if dst_cmd == "toggle":
+            action = cmd.split()[2]
+            if action == "up":
+                toggle("up")
+                await message.channel.send("開服")
+            elif action == "down":
+                toggle("down")
+                await message.channel.send("關服")
             return
-        if len(backup_message) > 15:
-            await message.channel.send("太長了")
+            
+        elif dst_cmd == "back_up":
+            backup_message = cmd.split()[2]
+            if ' ' in backup_message:
+                await message.channel.send("不能有空格")
+                return
+            if len(backup_message) > 15:
+                await message.channel.send("太長了")
+                return
+            status = back_up(backup_message)
+            if status == None:
+                await message.channel.send("備份好了")
+            else:
+                await message.channel.send("出包了")
             return
-        backup(backup_message)
-        await message.channel.send("備份好了")
-        return
+        
+        elif dst_cmd == "all_save":
+            await message.channel.send("全部存檔：")
+            await message.channel.send("\n".join(all_save()))
+            return
+        
+        elif dst_cmd == "roll_back":
+            if len(cmd.split()) == 3:
+                save = cmd.split()[2]
+                status = roll_back(save)
+                if not status == None:
+                    await message.channel.send("出包了")
+                else:
+                    await message.channel.send(f'roll back到"{save}"')
 
-    if "opgg" in cmd:
-        await message.channel.send("not implemented yet")
+            elif len(cmd.split()) == 2:
+                status = roll_back(None)
+                if not status == None:
+                    await message.channel.send("出包了")
+                else:
+                    await message.channel.send(f'roll back到最新ㄉ')
+
+    if len(cmd.split(' ')) == 2 and cmd.split()[0] == "opgg":
+        await message.channel.send("懶得寫")
         return
 
     await message.channel.send("建議不要亂打指令 :(")
